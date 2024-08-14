6271a4121961bd1615143e41e285bf58befba086
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-08-15 02:16:39 +0300

    added test for step1 valid case

--- main_test.go
+++ main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"os/exec"
+	"path"
+	"testing"
+)
+
+var testDir = "./tests"
+
+func TestStep1Valid(t *testing.T) {
+	t.Parallel()
+	cmdValid := exec.Command("go", "run", "main.go", path.Join(testDir, "step1", "valid.json"))
+	output, err := cmdValid.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	outputStr := string(output)
+	expectedOutput := "{}"
+	if outputStr != expectedOutput {
+		t.Errorf("Expected %s but got %s", expectedOutput, outputStr)
+	}
+}
