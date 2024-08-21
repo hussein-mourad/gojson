c2f66ffb7a0893bd47402a23254305c5e43a80d5
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-08-21 07:48:31 +0300

    added support for line, col for tokens
    This provides better error messages

--- main.go
+++ main.go
@@ -23,34 +23,38 @@ const (
 )
 
 type Token struct {
-	Type  string
-	Value string
+	Type   string
+	Value  string
+	Line   int
+	Column int
 }
 
-func NewToken(Type string, Value string) *Token {
-	return &Token{Type, Value}
+func NewToken(Type string, Value string, Line int, Column int) *Token {
+	return &Token{Type, Value, Line, Column}
 }
 
 func (t *Token) String() string {
-	return fmt.Sprintf("Type: %v\tValue: %v", t.Type, t.Value)
+	return fmt.Sprintf("Type: %v\tValue: %v\tLine: %v\tColumn: %v", t.Type, t.Value, t.Line, t.Column)
 }
 
 type Lexer struct {
 	input       string // code
 	pos         int    // position
 	currentRune rune   // current character
+	line        int    // current line
+	column      int    // current column
 }
 
 func NewLexer(input string) *Lexer {
-	l := &Lexer{input: input}
+	l := &Lexer{input: input, line: 1}
 	l.readRune()
 	return l
 }
 
 func (l *Lexer) makeToken(Type string) *Token {
-	value := string(l.currentRune)
+	value, line, column := string(l.currentRune), l.line, l.column
 	l.readRune()
-	return NewToken(Type, value)
+	return NewToken(Type, value, line, column)
 }
 
 func (l *Lexer) readRune() {
@@ -59,31 +63,44 @@ func (l *Lexer) readRune() {
 	} else {
 		l.currentRune = rune(l.input[l.pos])
 	}
+
+	// fmt.Printf("ch: %q\tline: %v\n", l.currentRune, l.line)
+
+	if l.currentRune == '\n' {
+		l.line++
+		l.column = 0
+	} else {
+		l.column++
+	}
+
 	l.pos++
 }
 
 func (l *Lexer) readString() *Token {
 	var str string
+	line, column := l.line, l.column
 	l.readRune() // skip opening qoute
 	for l.currentRune != '"' && l.currentRune != 0 {
 		str += string(l.currentRune)
 		l.readRune()
 	}
 	l.readRune() // skip closing quote
-	return NewToken(STRING, str)
+	return NewToken(STRING, str, line, column)
 }
 
 func (l *Lexer) readNumber() *Token {
 	var str string
+	line, column := l.line, l.column
 	for unicode.IsDigit(l.currentRune) || l.currentRune == '-' || l.currentRune == '.' {
 		str += string(l.currentRune)
 		l.readRune()
 	}
-	return NewToken(NUMBER, str)
+	return NewToken(NUMBER, str, line, column)
 }
 
 func (l *Lexer) readStringWithoutQuotes() *Token {
 	var str string
+	line, column := l.line, l.column
 	tokenType := UNKOWN
 	// '\n' is for handling last record that may not have a comma
 	for l.currentRune != ',' && l.currentRune != '\n' && l.currentRune != 0 {
@@ -98,7 +115,7 @@ func (l *Lexer) readStringWithoutQuotes() *Token {
 	case "null":
 		tokenType = NULL
 	}
-	return NewToken(tokenType, str)
+	return NewToken(tokenType, str, line, column)
 }
 
 func (l *Lexer) skipWhitespace() {
@@ -134,7 +151,7 @@ func (l *Lexer) NextToken() *Token {
 	}
 
 	if l.currentRune == 0 {
-		return NewToken(EOF, "")
+		return NewToken(EOF, "", l.line, l.column)
 	}
 
 	return l.makeToken(UNKOWN)
