b9072aa49f2cc11209a332b2d242b613acc16b6b
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-09-09 05:28:14 +0300

    added util functions to parser

--- parser/parser.go
+++ parser/parser.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/hussein-mourad/go-json-parser/ast"
 	"github.com/hussein-mourad/go-json-parser/lexer"
 )
 
@@ -13,46 +14,18 @@ var logger = log.New(os.Stdout, "", 0)
 
 type Parser struct {
 	lexer        *lexer.Lexer
-	currentToken lexer.Token // current Token
+	currentToken *lexer.Token // current Token
 }
 
 func NewParser(lexer *lexer.Lexer) *Parser {
 	p := &Parser{lexer: lexer}
-	p.nextToken()
 	return p
 }
 
-func (p *Parser) nextToken() {
-	p.currentToken = *p.lexer.NextToken()
-	fmt.Println(p.currentToken) // Debug
-	// if p.curToken.Type == UNKOWN {
-	// 	logger.Fatalf("error: unexpected token %v at line %v column %v\n", p.curToken.Value, p.curToken.Line, p.curToken.Column)
-	// }
-}
-
-func (p *Parser) parseNumber() interface{} {
-	s := p.currentToken.Value
-
-	// Try to parse as int
-	if intVal, err := strconv.Atoi(s); err == nil {
-		if len(strconv.Itoa(intVal)) != len(s) {
-			// Check if number doesn't have leading zeros
-			logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.currentToken.Line, p.currentToken.Column)
-		}
-		p.nextToken()
-		return intVal
-	}
-	// Try to parse as float
-	if floatVal, err := strconv.ParseFloat(s, 64); err == nil {
-		p.nextToken()
-		return floatVal
-	}
-
-	logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.currentToken.Line, p.currentToken.Column)
-	return nil
-}
-
 func (p *Parser) Parse() interface{} {
+	// document := ast.NewDocument()
+	p.eat()
+
 	// JSON must be an array or an object
 	if p.currentToken.Type != lexer.LBRACE && p.currentToken.Type != lexer.LBRACKET {
 		logger.Fatalf("error: unexpected %v at line: %v column: %v", p.currentToken.Value, p.currentToken.Line, p.currentToken.Column)
@@ -65,66 +38,67 @@ func (p *Parser) Parse() interface{} {
 }
 
 func (p *Parser) parseValue() interface{} {
-	switch p.currentToken.Type {
-	case lexer.STRING:
-		value := p.currentToken.Value
-		p.nextToken()
-		return value
-	case lexer.NUMBER:
-		return p.parseNumber()
+	switch p.at().Type {
 	case lexer.LBRACE:
 		return p.parseObject()
 	case lexer.LBRACKET:
 		return p.parseArray()
+	case lexer.STRING:
+		value := p.at().Value
+		p.eat()
+		return value
+	case lexer.NUMBER:
+		return p.parseNumber()
 	case lexer.BOOLEAN:
-		p.nextToken()
-		// TODO: parse false
-		return true
+		return p.parseBoolean()
 	case lexer.NULL:
-		p.nextToken()
+		p.eat()
 		return nil
 	}
 	return nil
 }
 
-func (p *Parser) parseObject() interface{} {
-	obj := make(map[string]interface{})
-	p.nextToken() // Skip opening brace
-	for p.currentToken.Type != lexer.RBRACE {
-		key := p.currentToken.Value
-		p.nextToken()
-
-		if p.currentToken.Value != ":" {
-			logger.Fatalf("error: expected : at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
-		}
-		p.nextToken()
-
-		value := p.parseValue()
-
-		obj[key] = value
-
-		if p.currentToken.Type == lexer.COMMA {
-			p.nextToken()
-			if p.currentToken.Type == lexer.RBRACE {
-				logger.Fatalf("error: unexpected , at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
-			}
-		}
-	}
-	p.nextToken() // Skip closing brace
+func (p *Parser) parseObject() *ast.Object {
+	obj := ast.NewObject()
+	p.eat() // Skip opening brace
+	// for p.at().Type != lexer.RBRACE {
+	// 	if p.isEOF() {
+	// 		// TODO: Print Error
+	// 	}
+	// 	key := p.eat().Value
+	//
+	// 	if p.currentToken.Type != lexer.COMMA {
+	// 		logger.Fatalf("Error: expected : at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
+	// 	}
+	// 	p.eat()
+	//
+	// 	value := p.parseValue()
+	//
+	// 	obj[key] = value
+	//
+	// 	if p.currentToken.Type == lexer.COMMA {
+	// 		p.eat()
+	// 		if p.currentToken.Type == lexer.RBRACE {
+	// 			logger.Fatalf("error: unexpected , at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
+	// 		}
+	// 	}
+	// }
+	// p.eat() // Skip closing brace
+	// return obj
 	return obj
 }
 
 func (p *Parser) parseArray() interface{} {
 	// TODO: Fix Parsing array eof issues, check pass2.json test
 	var arr []interface{}
-	p.nextToken() // skip opening bracket
+	p.eat() // skip opening bracket
 
 	for p.currentToken.Type != lexer.RBRACKET {
 		value := p.parseValue()
 		arr = append(arr, value)
 
 		fmt.Printf("p.curToken: %v\n", p.currentToken.String())
-		p.nextToken()
+		p.eat()
 
 		if p.currentToken.Type != lexer.COMMA && p.currentToken.Type != lexer.RBRACKET && p.currentToken.Type != lexer.EOF {
 			logger.Fatalf("error: unexpected %v at line: %v column: %v", p.currentToken.Value, p.currentToken.Line, p.currentToken.Column)
@@ -133,7 +107,7 @@ func (p *Parser) parseArray() interface{} {
 		// Handle extra comma before the end of the array
 		if p.currentToken.Type == lexer.COMMA {
 			// Move to the next token after the comma
-			p.nextToken()
+			p.eat()
 
 			// After a comma, there should be either a value or the end of the array
 			if p.currentToken.Type == lexer.RBRACKET {
@@ -149,6 +123,54 @@ func (p *Parser) parseArray() interface{} {
 		}
 	}
 
-	p.nextToken() // skip closing bracket
+	p.eat() // skip closing bracket
 	return arr
+}
+
+func (p *Parser) parseNumber() interface{} {
+	s := p.currentToken.Value
+
+	// Try to parse as int
+	if intVal, err := strconv.Atoi(s); err == nil {
+		if len(strconv.Itoa(intVal)) != len(s) {
+			// Check if number doesn't have leading zeros
+			logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.currentToken.Line, p.currentToken.Column)
+		}
+		p.eat()
+		return intVal
+	}
+	// Try to parse as float
+	if floatVal, err := strconv.ParseFloat(s, 64); err == nil {
+		p.eat()
+		return floatVal
+	}
+
+	logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.currentToken.Line, p.currentToken.Column)
+	return nil
+}
+
+func (p *Parser) parseBoolean() bool {
+	return p.eat().Value == "true"
+}
+
+func (p *Parser) isEOF() bool {
+	return p.currentToken.Type == lexer.EOF
+}
+
+func (p *Parser) at() *lexer.Token {
+	return p.currentToken
+}
+
+func (p *Parser) eat() *lexer.Token {
+	// Returns the previous token and then advances
+	prev := p.currentToken
+	p.currentToken = p.lexer.NextToken()
+	fmt.Println(p.currentToken) // FIXME: debugging only
+	return prev
+}
+
+func (p *Parser) expect(Type lexer.TokenType, err string) {
+	if p.at().Type != Type {
+		log.Fatalf("Error: %v on line %v, column %v", err, p.at().Line, p.at().Column)
+	}
 }
