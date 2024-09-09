2dbd4cf15ca962948340a65dbe9b80f8f2698ae2
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-09-09 03:29:33 +0300

    tests: exclude final in tests

--- test.sh
+++ test.sh
@@ -2,5 +2,12 @@
 
 for file in testdata/**/*.json; do
   echo "$file"
-  go run . "$file"
+  case "$file" in
+  *"final"*)
+    echo "final files aren't used"
+    ;;
+  *)
+    go run . "$file"
+    ;;
+  esac
 done
