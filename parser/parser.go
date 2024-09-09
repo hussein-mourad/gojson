d034481e1f67d73fec2516bcfa4c408f8ebdb4f7
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-09-09 04:34:18 +0300

    variable rename

--- parser/parser.go
+++ parser/parser.go
@@ -12,8 +12,8 @@ import (
 var logger = log.New(os.Stdout, "", 0)
 
 type Parser struct {
-	lexer    *lexer.Lexer
-	curToken lexer.Token // current Token
+	lexer        *lexer.Lexer
+	currentToken lexer.Token // current Token
 }
 
 func NewParser(lexer *lexer.Lexer) *Parser {
@@ -23,21 +23,21 @@ func NewParser(lexer *lexer.Lexer) *Parser {
 }
 
 func (p *Parser) nextToken() {
-	p.curToken = *p.lexer.NextToken()
-	fmt.Println(p.curToken) // Debug
+	p.currentToken = *p.lexer.NextToken()
+	fmt.Println(p.currentToken) // Debug
 	// if p.curToken.Type == UNKOWN {
 	// 	logger.Fatalf("error: unexpected token %v at line %v column %v\n", p.curToken.Value, p.curToken.Line, p.curToken.Column)
 	// }
 }
 
 func (p *Parser) parseNumber() interface{} {
-	s := p.curToken.Value
+	s := p.currentToken.Value
 
 	// Try to parse as int
 	if intVal, err := strconv.Atoi(s); err == nil {
 		if len(strconv.Itoa(intVal)) != len(s) {
 			// Check if number doesn't have leading zeros
-			logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.curToken.Line, p.curToken.Column)
+			logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.currentToken.Line, p.currentToken.Column)
 		}
 		p.nextToken()
 		return intVal
@@ -48,26 +48,26 @@ func (p *Parser) parseNumber() interface{} {
 		return floatVal
 	}
 
-	logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.curToken.Line, p.curToken.Column)
+	logger.Fatalf("error: invalid number %v at line: %v column: %v", s, p.currentToken.Line, p.currentToken.Column)
 	return nil
 }
 
 func (p *Parser) Parse() interface{} {
 	// JSON must be an array or an object
-	if p.curToken.Type != lexer.LBRACE && p.curToken.Type != lexer.LBRACKET {
-		logger.Fatalf("error: unexpected %v at line: %v column: %v", p.curToken.Value, p.curToken.Line, p.curToken.Column)
+	if p.currentToken.Type != lexer.LBRACE && p.currentToken.Type != lexer.LBRACKET {
+		logger.Fatalf("error: unexpected %v at line: %v column: %v", p.currentToken.Value, p.currentToken.Line, p.currentToken.Column)
 	}
 	value := p.parseValue()
 	if value == nil {
-		logger.Fatalf("error: unexpected %v at line: %v column: %v", p.curToken.Value, p.curToken.Line, p.curToken.Column)
+		logger.Fatalf("error: unexpected %v at line: %v column: %v", p.currentToken.Value, p.currentToken.Line, p.currentToken.Column)
 	}
 	return value
 }
 
 func (p *Parser) parseValue() interface{} {
-	switch p.curToken.Type {
+	switch p.currentToken.Type {
 	case lexer.STRING:
-		value := p.curToken.Value
+		value := p.currentToken.Value
 		p.nextToken()
 		return value
 	case lexer.NUMBER:
@@ -90,12 +90,12 @@ func (p *Parser) parseValue() interface{} {
 func (p *Parser) parseObject() interface{} {
 	obj := make(map[string]interface{})
 	p.nextToken() // Skip opening brace
-	for p.curToken.Type != lexer.RBRACE {
-		key := p.curToken.Value
+	for p.currentToken.Type != lexer.RBRACE {
+		key := p.currentToken.Value
 		p.nextToken()
 
-		if p.curToken.Value != ":" {
-			logger.Fatalf("error: expected : at line: %v column: %v", p.curToken.Line, p.curToken.Column)
+		if p.currentToken.Value != ":" {
+			logger.Fatalf("error: expected : at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
 		}
 		p.nextToken()
 
@@ -103,10 +103,10 @@ func (p *Parser) parseObject() interface{} {
 
 		obj[key] = value
 
-		if p.curToken.Type == lexer.COMMA {
+		if p.currentToken.Type == lexer.COMMA {
 			p.nextToken()
-			if p.curToken.Type == lexer.RBRACE {
-				logger.Fatalf("error: unexpected , at line: %v column: %v", p.curToken.Line, p.curToken.Column)
+			if p.currentToken.Type == lexer.RBRACE {
+				logger.Fatalf("error: unexpected , at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
 			}
 		}
 	}
@@ -119,32 +119,32 @@ func (p *Parser) parseArray() interface{} {
 	var arr []interface{}
 	p.nextToken() // skip opening bracket
 
-	for p.curToken.Type != lexer.RBRACKET {
+	for p.currentToken.Type != lexer.RBRACKET {
 		value := p.parseValue()
 		arr = append(arr, value)
 
-		fmt.Printf("p.curToken: %v\n", p.curToken.String())
+		fmt.Printf("p.curToken: %v\n", p.currentToken.String())
 		p.nextToken()
 
-		if p.curToken.Type != lexer.COMMA && p.curToken.Type != lexer.RBRACKET && p.curToken.Type != lexer.EOF {
-			logger.Fatalf("error: unexpected %v at line: %v column: %v", p.curToken.Value, p.curToken.Line, p.curToken.Column)
+		if p.currentToken.Type != lexer.COMMA && p.currentToken.Type != lexer.RBRACKET && p.currentToken.Type != lexer.EOF {
+			logger.Fatalf("error: unexpected %v at line: %v column: %v", p.currentToken.Value, p.currentToken.Line, p.currentToken.Column)
 		}
 
 		// Handle extra comma before the end of the array
-		if p.curToken.Type == lexer.COMMA {
+		if p.currentToken.Type == lexer.COMMA {
 			// Move to the next token after the comma
 			p.nextToken()
 
 			// After a comma, there should be either a value or the end of the array
-			if p.curToken.Type == lexer.RBRACKET {
-				logger.Fatalf("error: unexpected %v after comma at line: %v column: %v", p.curToken.Value, p.curToken.Line, p.curToken.Column)
+			if p.currentToken.Type == lexer.RBRACKET {
+				logger.Fatalf("error: unexpected %v after comma at line: %v column: %v", p.currentToken.Value, p.currentToken.Line, p.currentToken.Column)
 			}
 		}
 
 		// If the token is lexer.EOF, handle it appropriately
-		if p.curToken.Type == lexer.EOF {
+		if p.currentToken.Type == lexer.EOF {
 			// End of file without closing bracket
-			logger.Fatalf("error: unexpected end of file while parsing array at line: %v column: %v", p.curToken.Line, p.curToken.Column)
+			logger.Fatalf("error: unexpected end of file while parsing array at line: %v column: %v", p.currentToken.Line, p.currentToken.Column)
 			return arr // Or handle the error based on your needs
 		}
 	}
