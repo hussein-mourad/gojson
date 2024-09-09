14fa507412d81c01a8d742a8247a150b4633b68a
hussein-mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-09-09 03:26:36 +0300

    added utils.go

---
+++ utils.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func PosToLineColumn(input string) (int, int) {
+	// Converts pos in string to line and column
+	fmt.Printf("input: %q\n", input)
+	pos := 14
+	line, column := 1, 1
+	for i := 0; i < pos; i++ {
+		if pos >= len(input) {
+			fmt.Println("Error: Out of bound")
+			break
+		}
+		char := input[i]
+		if char == '\n' {
+			line++
+			column = 1
+		} else {
+			column++
+		}
+	}
+	return line, column
+}
